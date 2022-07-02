a062cb121783299c178795adf926fedfc8d49b71
MrsSprinkle <artealwright@gmail.com>
2022-07-02 16:30:01 -0400

    index.js content to server.js start script back

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server');
+const axios = require('axios');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -6,5 +7,36 @@ const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+
+
+(function() {
+
+    // Get Third party API
+    axios({
+      method: "GET", 
+      url:"https://artea-server-app.herokuapp.com/users"
+    })
+    .then(res => {
+      console.log(res);
+  })
+  // .then(() => {
+  
+  //   // POST Third party info to JSON-server
+  //   axios({
+  //     method:"POST", 
+  //     url:"https://artea-server-app.herokuapp.com/users", 
+  //     data:{
+  //       "id": 2,
+  //       "first_name": "Artea Wright"
+  //     }, 
+  //     headers: {
+  //       "Content-Type":"application/json"
+  //     }
+  //   })
+  // })
+  .catch(function (error) {
+      console.error(error);
+  });
+  }());
 
 server.listen(port);
