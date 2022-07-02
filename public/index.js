8765f343061dac60bc63d3fef782e34ed77514ad
MrsSprinkle <artealwright@gmail.com>
2022-07-01 21:21:49 -0400

    add index.js

---
+++ public/index.js
@@ -0,0 +1,33 @@
+const { default: axios } = require("axios");
+
+(function() {
+
+  // Get Third party API
+  axios({
+    method: "GET", 
+    url:"https://artea-server-app.herokuapp.com/users"
+  })
+  .then(res => {
+    console.log(res);
+})
+.then(() => {
+
+  // POST Third party info to JSON-server
+  axios({
+    method:"POST", 
+    url:"https://artea-server-app.herokuapp.com/users", 
+    data:{
+      "id": 6,
+      "first_name": "Artea Wright"
+    }, 
+    headers: {
+      "Content-Type":"application/json"
+    }
+  })
+})
+
+.catch(function (error) {
+	console.error(error);
+});
+
+}());
