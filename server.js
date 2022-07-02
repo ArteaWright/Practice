0e336bae7f9bf4bce3feecb136a63cbd7a8f7c4a
MrsSprinkle <artealwright@gmail.com>
2022-07-02 18:16:55 -0400

    adding express and insert axios

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const jsonServer = require('json-server');
 const axios = require('axios');
+const express = require('express');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+
+const app = express();
 
 server.use(middlewares);
 server.use(router);
@@ -13,30 +16,20 @@ server.listen(port);
 (function() {
 
     // Get Third party API
-    axios({
-      method: "GET", 
-      url:"https://artea-server-app.herokuapp.com/users"
+    app.get('/', (req, res) => {
+
+      axios({
+        method: "GET", 
+        url:"https://npiregistry.cms.hhs.gov/api/?number=&enumeration_type=&taxonomy_description=Obstetrics+%26+Gynecology&first_name=&use_first_name_alias=&last_name=&organization_name=&address_purpose=&city=charlotte&state=nc&postal_code=&country_code=&limit=1&skip=&pretty=on&version=2.1"
+      })
+      .then(res => {
+        var data = res;
+        console.log({data});
     })
-    .then(res => {
-      var data = res;
-      console.log({data});
-  })
-  // .then(() => {
-  
-  //   // POST Third party info to JSON-server
-  //   axios({
-  //     method:"POST", 
-  //     url:"https://artea-server-app.herokuapp.com/users", 
-  //     data:{
-  //       "id": 2,
-  //       "first_name": "Artea Wright"
-  //     }, 
-  //     headers: {
-  //       "Content-Type":"application/json"
-  //     }
-  //   })
-  // })
-  .catch(function (error) {
-      console.error(error);
-  });
+    .catch(function (error) {
+        console.error(error);
+    });
+
+    })
+
   }());
