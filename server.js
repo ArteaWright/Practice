b2c5081ee9d7b54a45099b3b3f82824fa0b78eae
MrsSprinkle <artealwright@gmail.com>
2022-07-02 19:06:22 -0400

    change to data

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ server.listen(port);
       url:"https://npiregistry.cms.hhs.gov/api/?number=&enumeration_type=&taxonomy_description=Obstetrics+%26+Gynecology&first_name=&use_first_name_alias=&last_name=&organization_name=&address_purpose=&city=charlotte&state=nc&postal_code=&country_code=&limit=1&skip=&pretty=on&version=2.1"
     })
     .then(res => {
-      const info = res.data.results;
-      console.log(info);
+      const info = res.data;
+      console.log(info)
   })
   // .then((info) => {
 
