be3e241bd8cbfa9e800ce2db51887aae8388ab91
MrsSprinkle <artealwright@gmail.com>
2022-07-02 18:56:24 -0400

    pause post request

--- server.js
+++ server.js
@@ -18,20 +18,21 @@ server.listen(port);
       url:"https://npiregistry.cms.hhs.gov/api/?number=&enumeration_type=&taxonomy_description=Obstetrics+%26+Gynecology&first_name=&use_first_name_alias=&last_name=&organization_name=&address_purpose=&city=charlotte&state=nc&postal_code=&country_code=&limit=1&skip=&pretty=on&version=2.1"
     })
     .then(res => {
-      return info = res;
+      return info = res.data.results;
       
-  }).then((info) => {
-
-    //  POST Third party info to JSON-server
-    axios({
-      method:"POST", 
-      url:"https://artea-server-app.herokuapp.com/users", 
-      data:{info}, 
-      headers: {
-        "Content-Type":"application/json"
-      }
-    })
   })
+  // .then((info) => {
+
+  //   //  POST Third party info to JSON-server
+  //   axios({
+  //     method:"POST", 
+  //     url:"https://artea-server-app.herokuapp.com/users", 
+  //     data:{info}, 
+  //     headers: {
+  //       "Content-Type":"application/json"
+  //     }
+  //   })
+  // })
   .catch(function (error) {
       console.error(error);
   });
