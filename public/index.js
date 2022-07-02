3a2e3378ace61d5f4a266d97924e5d4ac76e14cb
MrsSprinkle <artealwright@gmail.com>
2022-07-02 16:17:01 -0400

    comment out post request and reset json

--- public/index.js
+++ public/index.js
@@ -9,21 +9,21 @@
   .then(res => {
     console.log(res);
 })
-.then(() => {
+// .then(() => {
 
-  // POST Third party info to JSON-server
-  axios({
-    method:"POST", 
-    url:"https://artea-server-app.herokuapp.com/users", 
-    data:{
-      "id": 6,
-      "first_name": "Artea Wright"
-    }, 
-    headers: {
-      "Content-Type":"application/json"
-    }
-  })
-})
+//   // POST Third party info to JSON-server
+//   axios({
+//     method:"POST", 
+//     url:"https://artea-server-app.herokuapp.com/users", 
+//     data:{
+//       "id": 2,
+//       "first_name": "Artea Wright"
+//     }, 
+//     headers: {
+//       "Content-Type":"application/json"
+//     }
+//   })
+// })
 .catch(function (error) {
 	console.error(error);
 });
