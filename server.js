9fbe1b9d64158c7433c38bfc24a1b97e995e4810
MrsSprinkle <artealwright@gmail.com>
2022-07-02 18:42:55 -0400

    name change

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ server.listen(port);
       url:"https://npiregistry.cms.hhs.gov/api/?number=&enumeration_type=&taxonomy_description=Obstetrics+%26+Gynecology&first_name=&use_first_name_alias=&last_name=&organization_name=&address_purpose=&city=charlotte&state=nc&postal_code=&country_code=&limit=1&skip=&pretty=on&version=2.1"
     })
     .then(res => {
-      return data = res;
+      return info = res;
       
-  }).then((data) => {
+  }).then((info) => {
 
     //  POST Third party info to JSON-server
     axios({
       method:"POST", 
       url:"https://artea-server-app.herokuapp.com/users", 
-      data:{data}, 
+      data:{info}, 
       headers: {
         "Content-Type":"application/json"
       }
