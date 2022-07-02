49305a7a724fdf12c11722d3c018a1a2ef8ea216
MrsSprinkle <artealwright@gmail.com>
2022-07-02 19:57:38 -0400

    testing fetch request from UI

--- public/index.js
+++ public/index.js
@@ -1,1 +1,20 @@
-console.log("test");
+//  fetch data from Server 
+const getDoctors = async () => {
+    let url = "https://artea-server-app.herokuapp.com/users";
+
+    const response = await fetch(url);
+    const info = await response.json();
+
+    console.log(info);
+        
+    });
+}
+
+
+
+
+
+
+
+
+window.addEventListener("DOMContentLoaded", () => getDoctors)
