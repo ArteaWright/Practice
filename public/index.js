5414f521983fb55f2be87b6d455a7384fe88cf0b
MrsSprinkle <artealwright@gmail.com>
2022-07-02 20:21:56 -0400

    remove index 0

--- public/index.js
+++ public/index.js
@@ -8,7 +8,7 @@ const getDoctors = async () => {
     const info = await response;
 
     let template = '';
-    info[0].forEach(item => {
+    info.forEach(item => {
         template += `<div>${item.basic}</div>`
     })
 
