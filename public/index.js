024f268c59e691e0e7041750da11cda2e25f0a0c
MrsSprinkle <artealwright@gmail.com>
2022-07-02 20:38:52 -0400

    add more to tempate literal

--- public/index.js
+++ public/index.js
@@ -7,9 +7,19 @@ const getDoctors = async () => {
     const response = await fetch(url);
     const info = await response.json();
 
+    // perform DOM manipulation
     let template = '';
     info[0].map(item => {
-        template += `<div>${item.basic.first_name}</div>`
+        template += `<div>
+        <h3>${item.basic.first_name}</h3>
+        <h3>${item.basic.middle_name}</h3>
+        <h3>${item.basic.last_name}</h3>
+        <div>
+        <p>${item.basic.gender}</p>
+        <p>${item.basic.addresses}</p>
+        </div>
+        </div>
+        `
     })
 
     // Adding fetch content to HTML 
